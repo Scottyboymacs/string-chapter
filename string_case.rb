a44e9bee5b9f10209558ef9fc44ef8cde25e78fb
Scott McKeon <scott.mckeon@chicagobooth.edu>
2022-04-14 14:05:54 +0000

    Final Commit for "String" HW

--- string_case.rb
+++ string_case.rb
@@ -1,9 +1,15 @@
 # Output
-# 
+#
 # "HELLO friends AnD FaMiLy"
-# 
+#
 # using a combination of the upcase, downcase, and swapcase methods.
 
 greeting = "hello"
 people = " FRIENDS"
 other_people = " aNd fAmIlY"
+
+greeting = greeting.upcase
+people = people.downcase
+other_people = other_people.swapcase
+
+p greeting + people + other_people
